930af43b95b168395627ea1696f803b5b5b88102
LEE-Hun97 <leehun1997@naver.com>
2026-09-08 19:16:48 +0900

    5. bean validation에 맞게 어노테이션 및 값 추가

--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,14 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
     // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    @NotNull
     private String cardType;
+    @NotNull
     private Integer acquiredFloor;
 }
