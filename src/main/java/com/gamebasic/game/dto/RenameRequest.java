73bc0727db152f3e359e6417349741dcbe8a2f88
LEE-Hun97 <leehun1997@naver.com>
2026-09-09 13:00:22 +0900

    8-1. DTO를 추가하고 @Transcation을 추가하여서 이름 변경은 구현, delete는 현재 미구현

---
+++ src/main/java/com/gamebasic/game/dto/RenameRequest.java
@@ -0,0 +1,18 @@
+package com.gamebasic.game.dto;
+
+import com.gamebasic.runcard.dto.RunCardRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+import java.util.List;
+
+// 완성된 코드입니다. 수정하지 마세요.
+@Getter
+public class RenameRequest {
+    @NotBlank
+    @Size(min = 2, max = 12)
+    private String playerName;
+}
--- src/main/java/com/gamebasic/game/entity/Game.java
+++ src/main/java/com/gamebasic/game/entity/Game.java
@@ -62,6 +62,12 @@ public class Game {
         this.status = status;
     }
 
+    public void updateName(
+            String playerName
+    ){
+        this.playerName = playerName;
+    }
+
     public boolean isFinished() {
         return status != GameStatus.PLAYING;
     }
